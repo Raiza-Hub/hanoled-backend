768840ee7384f0f8b7ad73ec456a7a45048f61c8
Raiza-Hub <wisdomadebola62@gmail.com>
2025-09-20 12:25:41 +0100

    refactor: add /api/me route with session handling using Better Auth

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,55 @@
-import { toNodeHandler } from "better-auth/node";
 import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
-import { auth } from "./lib/auth";
-import { errorHandler } from "./middleware/errorHandler";
-import authRoutes from "./auth/auth.routes";
+import cors from "cors"
+import { errorHandler } from "./middleware/errorHandler.js";
+import { fromNodeHeaders, toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth.js";
+
 
 const app = express();
-
 const port = process.env.PORT;
 
-app.all("/api/auth", toNodeHandler(auth));
+app.all("/api/auth/{*splat}", toNodeHandler(auth));
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(errorHandler);
 
-app.use("/api/auth", authRoutes);
+
+app.get("/api/me", async (req, res) => {
+  try {
+    console.log('Received cookies:', req.headers.cookie);
+
+    const session = await auth.api.getSession({
+      headers: fromNodeHeaders(req.headers), // convert Express headers
+    });
+
+
+    if (!session) {
+      return res.status(401).json({ error: "Not authenticated" });
+    }
+
+    // session.user contains user details from Better Auth
+    return res.json({
+      success: true,
+      user: session.user,
+    });
+  } catch (err) {
+    console.error("Session error:", err);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Listening to requests on port ${port}`);
