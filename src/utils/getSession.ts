2eb3c664f2ccfb74737de26ba0939ef691a5206b
Raiza-Hub <wisdomadebola62@gmail.com>
2025-09-20 19:45:02 +0100

    fix: use fromNodeHeaders(req.headers) when fetching session

--- src/utils/getSession.ts
+++ src/utils/getSession.ts
@@ -1,15 +1,19 @@
-import { headers } from "next/headers";
-import { redirect } from "next/navigation";
-import { auth } from "../lib/auth";
-import { AppError } from "../utils/appError";
 
-export const getUserSession = async () => {
+import { AppError } from "./appError.js";
+import { redirect } from "next/navigation.js";
+import { fromNodeHeaders } from "better-auth/node";
+import { auth } from "@/lib/auth.js";
+import { Request } from "express";
+
+
+export const getUserSession = async (req: Request) => {
   console.log("getting user session");
 
   const session = await auth.api.getSession({
-    headers: await headers(),
+    headers: fromNodeHeaders(req.headers),
   });
 
+  
   if (!session) {
     console.log(new AppError("Please Sign In", 400));
     redirect("/sign-in");
